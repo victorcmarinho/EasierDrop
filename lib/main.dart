d9a675b25d7667e8cf8650f8d9a79c3db48be7d2
Victor Marinho <victorcmarinho@gmail.com.br>
2025-03-09 22:36:17 -0300

    chore: Optimize code

--- lib/main.dart
+++ lib/main.dart
@@ -17,22 +17,22 @@ Future<void> main() async {
 }
 
 Future<void> _configureWindow() async {
-  const WindowOptions windowOptions = WindowOptions(
-    minimumSize: Size(150, 150),
-    size: Size(250, 250),
-    backgroundColor: Colors.transparent,
-    skipTaskbar: false,
-    fullScreen: false,
-    alwaysOnTop: true,
-    titleBarStyle: TitleBarStyle.hidden,
-    title: 'Easier Drop',
+  await windowManager.ensureInitialized();
+
+  await windowManager.waitUntilReadyToShow(
+    const WindowOptions(
+      minimumSize: Size(150, 150),
+      size: Size(250, 250),
+      backgroundColor: Colors.transparent,
+      alwaysOnTop: true,
+      titleBarStyle: TitleBarStyle.hidden,
+      title: 'Easier Drop',
+    ),
+    () async {
+      await windowManager.show();
+      await windowManager.focus();
+    },
   );
-  await Future.wait([
-    windowManager.ensureInitialized(),
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await Future.wait([windowManager.show(), windowManager.focus()]);
-    }),
-  ]);
 }
 
 class EasierDrop extends StatelessWidget {
