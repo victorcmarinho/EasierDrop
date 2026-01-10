ad2af9020a656de915d36c5d53693f84f8d66d37
Victor Marinho <victorcmarinho@gmail.com.br>
2026-01-09 22:33:25 -0300

    Update Homebrew Cask to v1.1.1

--- Casks/easier-drop.rb
+++ Casks/easier-drop.rb
@@ -1,6 +1,6 @@
 cask "easier-drop" do
   version "1.1.1"
-  sha256 "284b027bf6f870cddca882bff52bfc40842ff5554f0b7d702a23be6be19ebbe6"
+  sha256 "e45cbfd3c0636b7c93cb4d271981ce544e517f24ff0ac58c5c4728883c9e859f"
 
   url "https://github.com/victorcmarinho/EasierDrop/releases/download/v#{version}/easier_drop-macos-universal-v#{version}.dmg"
   name "Easier Drop"
