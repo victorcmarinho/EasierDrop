3154c666fccee9e4ebda3984e9ee81b2b844a31e
Victor Marinho <victorcmarinho@gmail.com.br>
2026-02-28 21:53:05 -0300

    chore: Delete numerous test files and create settings.json.

--- test/config/env_config_coverage_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:easier_drop/config/env_config.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Env coverage boost', () {
-    Env.testCoverage();
-  });
-}
